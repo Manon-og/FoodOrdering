00723d030d0b63693489ae2d96d297692db56e68
Jhon Fabul <jgfabul@addu.edu.ph>
2024-06-15 21:13:16 +0800

    innit SENG Practice Expo Project

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -47,12 +47,7 @@ export default function TabLayout() {
           ),
         }}
       />
-      <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
+      <Tabs.Screen name="two" options={{ title: 'Tab Two', tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,}}
       />
     </Tabs>
   );
