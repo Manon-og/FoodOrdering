820c44e74e55231c218a900e2ef054dc575d0e19
siddigamon <115434776+siddigamon@users.noreply.github.com>
2024-10-12 17:42:28 +0800

    adding signin and logout email and pass

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,35 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-import Button from '../components/Button';
-import { Link } from 'expo-router';
+import { View, Text, ActivityIndicator } from "react-native";
+import React from "react";
+import Button from "../components/Button";
+import { Link, Redirect } from "expo-router";
+import { useAuth } from "@/providers/AuthProvider";
+import { supabase } from "@/lib/supabase";
 
 const index = () => {
+  const { session, loading } = useAuth();
+  console.log(session);
+
+  if (loading) {
+    <ActivityIndicator />;
+  }
+
+  if (!session) {
+    return <Redirect href={"/(auth)/sign-in"} />;
+  }
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', padding: 10 }}>
-      <Link href={'/(user)'} asChild>
+    <View style={{ flex: 1, justifyContent: "center", padding: 10 }}>
+      <Link href={"/(user)"} asChild>
         <Button text="Staff" />
       </Link>
-      <Link href={'/(admin)'} asChild>
+      <Link href={"/(admin)"} asChild>
         <Button text="Admin" />
       </Link>
+      <Link href={"/(auth)/sign-in"} asChild>
+        <Button text="Sign in" />
+      </Link>
+
+      <Button onPress={() => supabase.auth.signOut()} text="Sign out" />
     </View>
   );
 };
